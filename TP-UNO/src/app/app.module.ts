01a4bebaed8142cbe735322bb9ccbe18732cc5e7
Mauri <mauriminio96@gmail.com>
2017-09-03 20:51:49 -0300

    estilo catalogopeliculas avanzado, falta arreglar detalle del publico

--- TP-UNO/src/app/app.module.ts
+++ TP-UNO/src/app/app.module.ts
@@ -8,11 +8,14 @@ import { DetallesPeliculaComponent } from './detalles-pelicula/detalles-pelicula
 import { BarraFiltrosComponent } from './barra-filtros/barra-filtros.component';
 import { CatalogoPeliculasComponent } from './catalogo-peliculas/catalogo-peliculas.component';
 import { HttpModule }    from '@angular/http';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpModule,
+    Angular2FontawesomeModule,
   ],
 
   declarations: [               
