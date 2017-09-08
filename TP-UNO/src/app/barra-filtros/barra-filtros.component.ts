51815b1689e9aaa10d68e8e21fa72ed59fca8c8d
Gino <ginoemiliozzi11@gmail.com>
2017-09-07 21:46:19 -0300

    agrego Angular Material para combos select y los creo

--- TP-UNO/src/app/barra-filtros/barra-filtros.component.ts
+++ TP-UNO/src/app/barra-filtros/barra-filtros.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarraFiltrosComponent implements OnInit {
 
+    genres = [
+      {idGenre: '1', descGenre: 'Aventura'},
+      {idGenre: '2', descGenre: 'Acción'},
+      {idGenre: '3', descGenre: 'Supenso'}
+    ];
+    
+    years = [
+      {idyear: '2017'},
+      {idyear: '2016'},
+      {idyear: '2015'}
+    ];
+    
+    orderBys = [
+      {idOrder: 'Popularity'},
+      {idOrder: 'Starts'},
+      {idOrder: 'Views'}
+    ];
+
   constructor() { }
 
   ngOnInit() {
